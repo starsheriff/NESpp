880c9e1c19535cbf7b3ff8eb89324681c815ba4f
Johannes Weissmann <jo@starsheriff.eu>
2018-02-23 22:17:24 +0100

    initial setup

---
+++ makefile
@@ -0,0 +1,2 @@
+all::
+	clang++ -Wall -std=c++11 -lpthread -o target/NESpp src/main.cpp
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+class Cpu {
+public:
+    Cpu();
+    void run();
+    void hard_reset();
+    void cold_reset();
+
+private:
+    // special purpose registers
+    // todo: type? 8bit or 16bit? -> char or short?
+    int stack_pointer;
+    int program_counter;
+    int status_register;
+
+    // general purpose registers
+    char accumulator;     // 8-bit register
+    char index_register_x; // 8-bit register
+    char index_register_y; // 8-bit register
+
+    // processor status
+    // number of single bit flags --> model as enum?
+    // todo: bitfields -> p. 12 in NESDoc
+    char processor_status;
+};
+
+Cpu::Cpu() {
+};
+
+
+// todo:
+void Cpu::run() {
+    std::cout << "cpu running\n";
+}
+
+class Rom {
+public:
+    Rom(char* filepath);
+};
+
+int main(int argc, char** argv) {
+    Cpu cpu {};
+
+    cpu.run();
+}
